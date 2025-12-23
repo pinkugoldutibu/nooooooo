b8175addc6c18136b7838006490be74d1d590b9f
pinkugoldutibu <sharolarkansas@gmail.com>
2025-12-22 19:34:44 -0600

    Add files via upload

---
+++ skibbidi.py
@@ -0,0 +1,7 @@
+gmr = input("Do you want the great meam reset to happen?")
+if gmr=='yes':
+    print("Nice choice you are done with 67 and all ain't you?")
+else:
+    print("Nice choice must be brainrotted like me well same!!!")
+    no = input("What is you fav meme then?")
+    print("nice choice same")
